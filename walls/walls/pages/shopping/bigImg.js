24b425b52425443b36a5749a110159d9e93ce79e
tamarmiller2 <tamar88966@gmail.com>
2023-08-26 21:33:49 +0300

    first commit

---
+++ walls/walls/pages/shopping/bigImg.js
@@ -0,0 +1,18 @@
+﻿var myWrapper;
+
+//  גילוי התמונה הגדולה 
+
+function ShowBig(Obj) {
+    //אובייקט התמונה שנלחצה - Obj
+    myWrapper = document.querySelector('#Wrapper');   // X שמכיל את התמונה ואת ה div ה 
+    var myBigImg = document.querySelector('#BigImg');
+    myBigImg.src = Obj.getAttribute('src');  /*העתקת קובץ המקור מהתמונה שנלחצה*/
+    myWrapper.style.display = "inline"; /* X + הצגת האלמנט שמכיל תמונה */
+
+}
+
+//  הסתרת התמונה הגדולה
+
+function HideBig() {
+    myWrapper.style.display = "none";  /* X + הסתרת האלמנט שמכיל תמונה */
+}
---
+++ walls/walls/pages/shopping/stars/play.js
@@ -0,0 +1,22 @@
+﻿function PlayInLoad() {
+    //  מנגינה
+    var unmuteAudio = document.querySelector("#unmuteAudio");
+    unmuteAudio.addEventListener('click', function () { playStop("autoP1", "unmuteAudio") });
+    //  סרטון
+    var unmuteVideo = document.querySelector("#unmuteVideo");
+    unmuteVideo.addEventListener('click', function () { playStop("autoP2", "unmuteVideo") });
+
+}
+// mute  הפסק/הפעל  כפתור של 
+function playStop(audioId, btnId) {
+    var audio = document.querySelector("#" + audioId);
+    var TheBtn = document.querySelector("#" + btnId);
+    if (audio.muted == true) {
+        audio.muted = false;
+        TheBtn.textContent = "הפסק מנגינה";
+    }
+    else {
+        audio.muted = true;
+        TheBtn.textContent = "השמע מנגינה";
+    }
+}
